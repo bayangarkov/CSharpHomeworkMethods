e900b67408b0146fb6065b090847dde0b25d669e
bayangarkov <baiangarkov@gmail.com>
2017-02-05 22:43:21 +0200

    Reading string and print it

---
+++ 01-HelloName/HelloName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BlankReceipt
+{
+    class HelloName
+    {
+        static void Main()
+        {
+
+            var name = GetUserName();
+            Console.WriteLine($"Hello, {name}!");
+            
+        }
+
+        static string GetUserName()
+        {
+            var name = Console.ReadLine();
+            return name;
+        }
+    }
+}
