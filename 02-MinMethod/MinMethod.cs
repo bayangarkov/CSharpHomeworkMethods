dff6e7a6f419e73992564cad1cbd93051e460f20
bayangarkov <baiangarkov@gmail.com>
2017-02-05 23:09:02 +0200

    Finding min value from input

---
+++ 02-MinMethod/MinMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_MinMethod
+{
+    class MinMethod
+    {
+        static void Main()
+        {
+
+            var result = FindMaxValue();
+            Console.WriteLine(result);
+        }
+
+        static int FindMaxValue()
+        {
+            var minValue = int.MaxValue;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+
+                if (input < minValue)
+                {
+                    minValue = input;
+                }
+            }
+
+            return minValue;
+        }
+    }
+}
