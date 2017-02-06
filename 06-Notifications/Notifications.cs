ba0dd1810f82a36903c40621044d0e0e9b2c8e70
bayangarkov <baiangarkov@gmail.com>
2017-02-06 21:47:16 +0200

    Output a different message from difference input

---
+++ 06-Notifications/Notifications.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Notifications
+{
+    class Notifications
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string operation = String.Empty;
+            string message = String.Empty;
+            int code = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string successOrErrorMessage = Console.ReadLine();
+                if (successOrErrorMessage == "success")
+                {
+                    operation = Console.ReadLine();
+                    message = Console.ReadLine();
+                    var reason = String.Empty;
+
+                    var printedMessage = ShowSuccess(operation, message);
+                    Console.WriteLine(printedMessage);
+                }
+                else if (successOrErrorMessage == "error")
+                {
+                    operation = Console.ReadLine();
+                    code = int.Parse(Console.ReadLine());
+                    
+
+                    var printedMessage = ShowError(operation, code);
+                    Console.WriteLine(printedMessage);
+                }
+            }
+        }
+
+        static string ShowError(string operation, int code)
+        {
+            var reason = String.Empty;
+
+            if (code >= 0)
+            {
+                reason = "Invalid Client Data";
+            }
+            else
+            {
+                reason = "Internal System Failure";
+            }
+
+            var printedMessage = $"Error: Failed to execute {operation}.\n==============================\nError Code: {code}.\nReason: {reason}.";
+            return printedMessage;
+        }
+
+        static string ShowSuccess(string operation, string message)
+        {
+            var printedMessage = $"Successfully executed {operation}.\n==============================\nMessage: {message}.";
+            return printedMessage;
+        }
+
+    }
+}
