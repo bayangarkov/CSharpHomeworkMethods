adfd0eb5d8e02006b31eddde5fda5cfbd3e747ef
bayangarkov <baiangarkov@gmail.com>
2017-02-11 18:38:51 +0200

    Concatinating strings with chosen delimiter from input

---
+++ 07-TrickyStringBroken/TrickyStringBroken.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_TrickyStringBroken
+{
+    class TrickyStringBroken
+    {
+        static void Main()
+        {
+            var delimiter = Console.ReadLine();
+
+            var numberOfStrings = int.Parse(Console.ReadLine());
+
+            var result = string.Empty;
+
+            var currentString = string.Empty;
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                currentString = Console.ReadLine();
+
+                if (i == (numberOfStrings - 1))
+                {
+                    result += currentString;
+                }
+                else
+                {
+                    result = result + currentString + delimiter;
+                }
+                
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
