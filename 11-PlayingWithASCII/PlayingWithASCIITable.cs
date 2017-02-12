8ab683a577444f83932fc3581e76d615c50d9ea4
bayangarkov <baiangarkov@gmail.com>
2017-02-12 10:46:56 +0200

    Decrypting message from int number to letters

---
+++ 11-PlayingWithASCII/PlayingWithASCIITable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_PlayingWithASCII
+{
+    class PlayingWithASCIITable
+    {
+        static void Main()
+        {
+            int num = 76;
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("chetno");
+            }
+            else
+            {
+                Console.WriteLine("nechetno");
+            }
+        }
+    }
+}
